37ec0ac35452e20e197b86da5a7d895334d5bb94
Daniel-timmy <ajayitimmy45@gmail.com>
2024-04-17 16:06:13 +0100

    updated the events app

--- backend/ticketmaster/event/models.py
+++ backend/ticketmaster/event/models.py
@@ -1,7 +1,6 @@
 import uuid
 
 from django.conf import settings
-from django.contrib.auth import get_user_model
 from django.db import models
 from django.contrib.postgres.fields import ArrayField
 
@@ -16,12 +15,37 @@ class Event(models.Model):
     Suitable for applications such as an exclusive meeting,
     concerts, on-spot booking and the likes. Mainly ticketing.
     """
+    ONLINE = 'ON'
+    PHYSICAL = 'PH'
+    BOTH = 'BO'
+
+    EVENT_TYPES = [
+        (ONLINE, 'Online'),
+        (PHYSICAL, 'Physical'),
+        (BOTH, 'Both'),
+    ]
+    DAILY = 'DY'
+    WEEKLY = 'WK'
+    MONTHLY = 'MN'
+    ONE_TIME = 'OT'
+
+    RECURRING_MODE = [
+        (DAILY, 'Daily'),
+        (WEEKLY, 'Weekly'),
+        (MONTHLY, 'Monthly'),
+        (ONE_TIME, 'One time')
+    ]
+
     id = models.UUIDField(default=uuid.uuid4, unique=True,
                           primary_key=True, editable=False)
     creator = models.ForeignKey(settings.AUTH_USER_MODEL, default=None, on_delete=models.CASCADE)
     event_name = models.CharField(max_length=100)
     ticket_data = ArrayField(
         models.JSONField(), default=None)
+    # if it happens to be a recurring the start and end date will
+    # encompass the entire duration through which the event will keep
+    # repeating. The start and end time will apply to each of the instance
+    # the event will repeat
     start_date = models.DateField(default=None)
     start_time = models.TimeField(default=None)
     end_date = models.DateField(default=None)
@@ -29,9 +53,23 @@ class Event(models.Model):
     venue_name = models.CharField(max_length=100)
     venue_address = models.CharField(max_length=1000)
     venue_country = models.CharField(max_length=100)
-    online_event = models.BooleanField()
-    recurring_event = models.BooleanField()
-    description = models.CharField()
+    online_event = models.CharField(
+        max_length=2,
+        choices=EVENT_TYPES,
+        default=ONLINE,
+    )
+    recurring_event = models.CharField(
+        max_length=2,
+        choices=RECURRING_MODE,
+        default=ONE_TIME,
+    )
+    description = models.TextField()
+    registration_end_date = models.DateTimeField(null=True, blank=True)
+    # featured(public, private)
+    # theme/ custom flyer
+
+    def __str__(self):
+        return self.event_name
 
 
 class EventBooking(models.Model):
@@ -45,8 +83,7 @@ class EventBooking(models.Model):
     customer_name = models.CharField()
     email = models.EmailField(unique=True, default=None)
     qr_code = models.BinaryField(default=None)
-    start_date = models.DateField()
-    start_time = models.TimeField()
-    end_date = models.DateField()
-    end_time = models.TimeField()
-    event_type = models.CharField()
+    phone_number = models.PositiveIntegerField(max_length=12)
+    extra_info = ArrayField(
+        models.JSONField(), default=[])
+    # reminder options
